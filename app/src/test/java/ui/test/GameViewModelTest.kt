37f0caac42d0c7fa6ef9cf85d15553a3f0238022
Luis Mendes <machadoluismendes@gmail.com>
2023-09-09 15:01:03 -0300

    Increment
    data package
    WordsData class
    GameViewModelTest

---
+++ app/src/test/java/ui/test/GameViewModelTest.kt
@@ -0,0 +1,11 @@
+package ui.test
+
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset()  {
+    }
+}
